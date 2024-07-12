06e1a86d126e0278960fe8fdc93ca3b6f6d26415
myV <147030038+rifqichusaini@users.noreply.github.com>
2024-07-12 23:20:26 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var nama;
-nama = prompt("masukkan nama anda : ");
-if(nama == null){
-    alert("Halo selamat datang di website pribadi saya!");
-} else{
-    alert("Halo Mr."+nama+" selamat datang di website pribadi saya!");
-}
